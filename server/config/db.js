8514088f6b43f2b233959a063d829a67bab1dd1d
miznaansari <71661652+miznaansari@users.noreply.github.com>
2025-03-11 15:12:35 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/test1", { 
-      useNewUrlParser: true,
-    });
-    console.log(`MongoDB Connected: {conn.connection.host}`);
+    const conn = await mongoose.connect("mongodb+srv://zafarahmad:2a2q8mOQuvJg44ll@cluster0.zffii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { 
+    useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(MongoDB Connected: {conn.connection.host});
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
 }
 
-module.exports = connectDB;
+module.exports = connectDB;
